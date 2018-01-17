d24067692c953c1c697135891fc908554d846e94
forax <forax@univ-mlv.fr>
2018-01-17 23:32:11 +0100

    use ~

--- install-jdk10.sh
+++ install-jdk10.sh
@@ -12,12 +12,12 @@ JDK_BUILD="$(echo "${TMP}" | tr -d '[:space:]')"        # remove all whitespace
 
 JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
-mkdir -p jdk
-cd jdk
+mkdir ~/jdk
+cd ~/jdk
 wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
-export JAVA_HOME=./jdk/jdk-${JDK_FEATURE}
+export JAVA_HOME=~/jdk/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
-cd ..
+cd -
 
 ${JAVA_HOME}/bin/java --version
--- install-jlink-image.sh
+++ install-jlink-image.sh
@@ -2,10 +2,10 @@
 set -e
 
 # create a small jdk using jlink
-mkdir -p link_jdk
-${JAVA_HOME}/bin/jlink --add-modules java.base --output jlink_jdk
+mkdir -p ~/jlink_jdk
+${JAVA_HOME}/bin/jlink --add-modules java.base --output ~/jlink_jdk
 
-export JAVA_HOME=jlink_jdk
+export JAVA_HOME=~/jlink_jdk
 export PATH=${JAVA_HOME}/bin:$PATH
 
 ${JAVA_HOME}/bin/java --version
