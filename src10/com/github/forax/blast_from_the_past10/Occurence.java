e142f107febd135ede3c7d4c00460459fb6b00c0
forax <forax@univ-mlv.fr>
2018-01-17 20:24:27 +0100

    remove commented lines

--- src10/com/github/forax/blast_from_the_past10/Occurence.java
+++ src10/com/github/forax/blast_from_the_past10/Occurence.java
@@ -17,25 +17,10 @@ public class Occurence {
     
     Map<String, Long> map;
     try(var lines = Files.lines(path)) {
-      map = lines/*.parallel()*/
-                 .flatMap(pattern::splitAsStream)
+      map = lines.flatMap(pattern::splitAsStream)
                  .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
-    /*
-    var map = new HashMap<String, Long>();
-    try(var lines = Files.lines(path)) {
-      lines.flatMap(pattern::splitAsStream)
-           .forEach(word -> map.merge(word, 1L, Long::sum));
-    }*/
-    /*
-    var map2 = new HashMap<String, long[]>();
-    var map = (HashMap<String, Long>)(HashMap<String,?>)map2;
-    try(var lines = Files.lines(path)) {
-      lines.flatMap(pattern::splitAsStream)
-           .forEach(word -> map2.computeIfAbsent(word, __ -> new long[1])[0]++);
-    }
-    ((HashMap<String, Object>)(HashMap<String,?>)map2).replaceAll((__, value) -> Long.valueOf(((long[])value)[0]));
-    */
+    
     var list = new ArrayList<>(map.entrySet());
     list.sort(Map.Entry.<String,Long>comparingByValue().reversed());
     System.out.println(list);
