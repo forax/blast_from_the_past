8e0baf2bba81e83554127a445ae31f90b297047b
forax <forax@univ-mlv.fr>
2018-01-17 20:52:32 +0100

    install-jdk10 should create jdk folder if necessary

--- install-jdk10.sh
+++ install-jdk10.sh
@@ -12,12 +12,13 @@ JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
 
 JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
+mkdir jdk
 cd jdk
 wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
-export JAVA_HOME=~/jdk-${JDK_FEATURE}
+export JAVA_HOME=./jdk/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
-cd -
+cd ..
 
 java --version
 
