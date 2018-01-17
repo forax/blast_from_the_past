4f919ea9f904679e84e7416b6920f375650d5dd8
forax <forax@univ-mlv.fr>
2018-01-17 23:59:08 +0100

    remove unused import

--- src10/com/github/forax/blast_from_the_past10/Occurence.java
+++ src10/com/github/forax/blast_from_the_past10/Occurence.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.regex.Pattern;
