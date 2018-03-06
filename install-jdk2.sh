4d29746c7a20ec2d02764cd5412b9779b2abcfba
forax <forax@univ-mlv.fr>
2018-03-06 14:46:01 +0100

    travis: mount the folder jdk in docker

--- install-jdk2.sh
+++ install-jdk2.sh
@@ -5,7 +5,9 @@ mkdir ~/jdk
 cd ~/jdk
 wget http://igm.univ-mlv.fr/~forax/tmp/jdk/jdk-1_2_2_017-linux-i586.tar.gz
 tar -xzf jdk-1_2_2_017-linux-i586.tar.gz
-export JAVA_HOME=~/jdk/jdk1.2.2
 cd -
+#export JAVA_HOME=~/jdk/jdk1.2.2
+
+export JAVA_HOME=jdk/jdk1.2.2
 
 #${JAVA_HOME}/bin/java -version
